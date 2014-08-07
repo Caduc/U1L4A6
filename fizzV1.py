4c575bd75c59d536bdbabd3fb2753198fbf6781a
caduc <remaier@gmail.com>
2014-08-06 20:34:16 -0400

    second version with input options

--- fizzV1.py
+++ fizzV1.py
@@ -1,15 +1,15 @@
-
-
 import sys
+ 
+print "The name of this script is {}".format(sys.argv[0])
+print "User supplied {} arguments at run time".format(len(sys.argv))
+ 
+for arg in sys.argv[1:]:
+  print arg
 
-#  print "The name of this script is {}".format(sys.argv[0])
-#  print "User supplied {} arguments at run time".format(len(sys.argv))
-#  
-#  for arg in sys.argv[1:]:
-#    print arg
-
-top_num = 100
+#top_num = 100
 num = 0
+top_num = int(raw_input('Input upper value. '))
+
 
 while num < top_num:
 	num  += 1
